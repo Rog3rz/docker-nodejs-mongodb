a60c681546d67aa14f3fd0a5b1dddc439b1144f0
Victor Guillermo Ortiz Rios <vgortizr@Victors-MacBook-Air.local>
2025-01-06 10:08:55 -0600

    remove standard

--- Dockerfile
+++ Dockerfile
@@ -16,4 +16,4 @@ COPY . .
 
 EXPOSE 3000
 
-CMD ["pnpm", "start"]
+CMD ["pnpm", "dev"]
--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-import express from "express";
-import CONFIG_VARIABLES from "./config.js";
-import "./database.js";
+import express from 'express';
+import CONFIG_VARIABLES from './config.js';
+import './database.js';
 
 const { PORT } = CONFIG_VARIABLES;
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
 app.listen(PORT, () => {
-  console.log("Running on ", PORT);
+  console.log('Running on ', PORT);
 });
